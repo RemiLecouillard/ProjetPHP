28348c2c59d69768157e8cc90d3166822818878f
xavierefremont <xaviere.fremont@yahoo.fr>
2016-10-20 08:46:08 +0200

    dvp afficher

--- traiterAfficherCoureur.php
+++ traiterAfficherCoureur.php
@@ -1,3 +1,8 @@
 <?php
 include("fonctionOrac.php");
+if(isset($_POST["envoyer"])){
+	if(!empty($_POST["nom"]) || !empty($_POST["prenom"]) || !empty($_POST["anneeNais"]) || !empty($_POST["pays"]) || !empty($_POST["anneePrem"]) || !empty($_POST["dateInser"])){
+		$conn = OuvrirConnexion('ETU2_58', 'remixav16','info');
+	}
+}
 ?>
