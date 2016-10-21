7bf695bdf56adf639f9324aba27ac50b0d51d35f
xavierefremont <xaviere.fremont@yahoo.fr>
2016-10-21 13:46:05 +0200

    liste coureur

--- codeTdf.php
+++ codeTdf.php
@@ -1,15 +1,18 @@
 <?php
 	include("fonctionOrac.php");
-	
+	$conn = OuvrirConnexion('ETU2_58', 'remixav16','info');
+	$req = "SELECT distinct code_tdf FROM tdf_coureur";
 	$req = utf8_decode($req);
 	$cur = PreparerRequete($conn,$req);
 	$res = ExecuterRequete($cur);
 	$nb = LireDonnees1($cur,$donnees);
-	$conn = OuvrirConnexion('ETU2_58', 'remixav16','info');
-	$req = "SELECT distinct code_tdf FROM tdf_coureur";
 	
-	foreach($donnees as $cle=>$codeTdf){
-		echo "<option value=".$codeTdf.">".$codeTdf."</option>";
+	
+	
+	foreach($donnees as $cle=>$contenu){
+		foreach($contenu as $cle=$codeTdf){
+		echo "<option value=$codeTdf>$codeTdf</option>";
+		}
 	}
 
 
