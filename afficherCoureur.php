e6c4b2e8fee2bc6f431fb7880feb5685aa329a36
RemiLecouillard <remizbay@hotmail.fr>
2016-11-06 19:25:52 +0100

    resolution bug d'affichage des coureurs

--- afficherCoureur.php
+++ afficherCoureur.php
@@ -1,49 +1,49 @@
-﻿
+
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8"/>
-		<link rel="stylesheet" href="designForm.css" type="text/CSS" />
-	</head>
-	<body>
-		<?php include("menu.html");
-			include("fonctionOrac.php");
-			include("fonctionsUtiles.php");
-		?>
-		<div class="form-style-2">
-		<div class="form-style-2-heading">Rechercher un coureur</div>
-		<form method="post" action="<?php $_SERVER['PHP_SELF'] ?>" enctype="application/x-www-form-urlencoded" name="afficheCoureur"/>
-			<label for="nom"><span> Nom :</span> 
-			<input name="nom" id="nom" value="<?php verifierRempli('nom') ?>"/></label>
-			</br>
+  <head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="designForm.css" type="text/CSS" />
+  </head>
+  <body>
+    <?php include("menu.html");
+      include("fonctionOrac.php");
+      include("fonctionsUtiles.php");
+    ?>
+    <div class="form-style-2">
+    <div class="form-style-2-heading">Rechercher un coureur</div>
+    <form method="post" action="<?php $_SERVER['PHP_SELF'] ?>" enctype="application/x-www-form-urlencoded" name="afficheCoureur"/>
+      <label for="nom"><span> Nom :</span>
+      <input name="nom" id="nom" value="<?php verifierRempli('nom') ?>"/></label>
+      </br>
 
-			<label for="prenom"><span> Prenom :</span> 
-			<input name="prenom" id="prenom" value="<?php verifierRempli('prenom') ?>"/></label>
-			</br>
+      <label for="prenom"><span> Prenom :</span>
+      <input name="prenom" id="prenom" value="<?php verifierRempli('prenom') ?>"/></label>
+      </br>
 
-			<label for="annee_Naissance"><span> Année de Naissance :</span>
-			<input name="annee_Naissance" id="annee_Naissance" type="number" value="<?php verifierRempli('annee_Naissance') ?>" /> </label>
-			</br>
-			<label for="code_tdf"><span> Pays : </span>
-			<select name="code_tdf" id="code_tdf">
-				<option value="" selected>Pays</option>
-				<?php include("codeTdf.php"); ?>
-			</select></label>
-			</br>
+      <label for="annee_Naissance"><span> Année de Naissance :</span>
+      <input name="annee_Naissance" id="annee_Naissance" type="number" value="<?php verifierRempli('annee_Naissance') ?>" /> </label>
+      </br>
+      <label for="code_tdf"><span> Pays : </span>
+      <select name="code_tdf" id="code_tdf">
+        <option value="" selected>Pays</option>
+        <?php include("codeTdf.php"); ?>
+      </select></label>
+      </br>
 
-			<label for="annee_Prem"><span> Année du premier tour : </span>
-			<input name="annee_Prem" id="annee_Prem" type="number" value="<?php verifierRempli('annee_Prem') ?>"/></label>
-		
-			</br>
-			<label for="date_Insert">
-			<span>Date d'insertion :</span>
-			<input name="date_Insert" id="date_Insert" type="date" placeholder="jj/mm/aa" /></label>
-			</br>
-			
-			<input name="rechercher" id="rechercher" type="submit" value="rechercher"/>
-		</form>
-		</div>
-	
-		<?php include("traiterAfficherCoureur.php"); ?>
-	</body>
+      <label for="annee_Prem"><span> Année du premier tour : </span>
+      <input name="annee_Prem" id="annee_Prem" type="number" value="<?php verifierRempli('annee_Prem') ?>"/></label>
+    
+      </br>
+      <label for="date_Insert">
+      <span>Date d'insertion :</span>
+      <input name="date_Insert" id="date_Insert" type="date" placeholder="jj/mm/aa" /></label>
+      </br>
+      
+      <input name="rechercher" id="rechercher" type="submit" value="rechercher"/>
+    </form>
+    </div>
+  
+    <?php include("traiterAfficherCoureur.php"); ?>
+  </body>
 </html>
--- traiterAfficherCoureur.php
+++ traiterAfficherCoureur.php
@@ -1,86 +1,98 @@
 <?php
-
-function afficherListe($tab,$nbLignes){
-  echo "$nbLignes resultats trouvés<br />\n";
-  $ctr = 0;
-  if ($nbLignes > 0) 
-  {
-    echo "<table border=\"1\">\n";
-    echo "<tr>\n";
-    foreach ($tab as $key => $val)  // lecture des noms de colonnes
-    {
-      if($key == "N_COUREUR")
-       echo "<th style='display:none;'>$key</th>\n";
-      else
-        echo "<th>$key</th>\n";
-    }
-  echo "<th></th><th></th><th></th>";
-    echo "</tr>\n";
-    for ($i = 0; $i < $nbLignes; $i++) // balayage de toutes les lignes
-    {
+  include("fonctionsTraitement.php");
+  include("traiterNom.php");
+  include("traiterPrenom.php");
+  function afficherListe($tab,$nbLignes){
+    echo "$nbLignes resultats trouvés<br />\n";
     $ctr = 0;
-      echo "<form method='post' action='action.php' enctype='application/x-www-form-urlencoded' name='supp'><tr>\n";
-      foreach ($tab as $data) // lecture des enregistrements de chaque colonne
+    if ($nbLignes > 0)
     {
-    $data[$i] = utf8_encode($data[$i]);
-      if($ctr == 0){
-        echo '<input type="hidden" name="N_COUREUR" value="'.$data[$i].'"/>';
-      }
-      else if($ctr == 1)
-      echo '<input type="hidden" name="NOM" value="'.$data[$i].'"/>';
-      else if($ctr == 2)
-      echo '<input type="hidden" name="PRENOM" value="'.$data[$i].'"/>';
-      else if($ctr == 3)
-      echo '<input type="hidden" name="ANNEE_NAISSANCE" value="'.$data[$i].'"/>';
-      else if($ctr == 4)
-      echo '<input type="hidden" name="code_tdf" value="'.$data[$i].'"/>';
-      else if($ctr == 5)
-      echo '<input type="hidden" name="ANNEE_PREM" value="'.$data[$i].'"/>';
-      else if($ctr == 7)
-      echo '<input type="hidden" name="DATE_INSERT" value="'.$data[$i].'"/>';
-      if($ctr == 0)
-        echo "<td style='display:none;'>$data[$i]</td>\n";
-      else
-        echo "<td>$data[$i]</td>\n";
-    $ctr++;
-      }
-    echo "<td><input type='submit'value ='modifier' name='modif'/></td>";
-    echo "<td><input type='submit'value ='supprimer' name='supp'/></td>";
-    echo "<td><input type='submit'value ='visualiser' name='visualiser'/></td></tr>";
-    echo "</form>\n";
-    //include("voirToutCou.htm");
+      echo "<table border=\"1\">\n";
+      echo "<tr>\n";
+      foreach ($tab as $key => $val)  // lecture des noms de colonnes
+               {
+                 if($key == "N_COUREUR")
+                   echo "<th style='display:none;'>$key</th>\n";
+                 else
+                   echo "<th>$key</th>\n";
+               }
+      echo "<th></th><th></th><th></th>";
+      echo "</tr>\n";
+      for ($i = 0; $i < $nbLignes; $i++) // balayage de toutes les lignes
+           {
+             $ctr = 0;
+             echo "<form method='post' action='action.php' enctype='application/x-www-form-urlencoded' name='supp'><tr>\n";
+             foreach ($tab as $data) // lecture des enregistrements de chaque colonne
+                      {
+                        $data[$i] = utf8_encode($data[$i]);
+                        if($ctr == 0){
+                          echo '<input type="hidden" name="N_COUREUR" value="'.$data[$i].'"/>';
+                        }
+                        else if($ctr == 1)
+                          echo '<input type="hidden" name="NOM" value="'.$data[$i].'"/>';
+                        else if($ctr == 2)
+                          echo '<input type="hidden" name="PRENOM" value="'.$data[$i].'"/>';
+                        else if($ctr == 3)
+                          echo '<input type="hidden" name="ANNEE_NAISSANCE" value="'.$data[$i].'"/>';
+                        else if($ctr == 4)
+                          echo '<input type="hidden" name="code_tdf" value="'.$data[$i].'"/>';
+                        else if($ctr == 5)
+                          echo '<input type="hidden" name="ANNEE_PREM" value="'.$data[$i].'"/>';
+                        else if($ctr == 7)
+                          echo '<input type="hidden" name="DATE_INSERT" value="'.$data[$i].'"/>';
+                        if($ctr == 0)
+                          echo "<td style='display:none;'>$data[$i]</td>\n";
+                        else
+                          echo "<td>$data[$i]</td>\n";
+                        $ctr++;
+                      }
+             echo "<td><input type='submit'value ='modifier' name='modif'/></td>";
+             echo "<td><input type='submit'value ='supprimer' name='supp'/></td>";
+             echo "<td><input type='submit'value ='visualiser' name='visualiser'/></td></tr>";
+             echo "</form>\n";
+             //include("voirToutCou.htm");
+           }
+      echo "</table>\n";
     }
-    echo "</table>\n";
-  } 
-  else 
-  {
-    echo "Pas de ligne<br />\n";
-  } 
+    else
+    {
+      echo "Pas de ligne<br />\n";
+    }
+    
+  }
   
-}
-
-if(isset($_POST["rechercher"])){
-  if(!empty($_POST["nom"]) || !empty($_POST["prenom"]) || !empty($_POST["annee_Nais"]) || !empty($_POST["code_tdf"]) || !empty($_POST["annee_Prem"]) || !empty($_POST["date_Insert"])){
-    //$conn = OuvrirConnexion('ETU2_58', 'remixav16','info');
-    $req = "SELECT * FROM tdf_coureur where";
-    $i=0;
-    foreach($_POST as $cle=>$contenu){
-      if(!empty($_POST[$cle]) && $cle != "rechercher"){
-        if($i==0) $req= $req." $cle='$contenu'";
-        else $req = $req." and $cle='$contenu'";
-        $i++;
+  if(isset($_POST["rechercher"])){
+    if(!empty($_POST["nom"]) || !empty($_POST["prenom"]) || !empty($_POST["annee_Naissance"]) || !empty($_POST["code_tdf"]) || !empty($_POST["annee_Prem"]) || !empty($_POST["date_Insert"])){
+      //$conn = OuvrirConnexion('ETU2_58', 'remixav16','info');
+      $req = "SELECT * FROM tdf_coureur where ";
+      $i=0;
+      
+      foreach($_POST as $cle=>$contenu){
+        if(!empty($_POST[$cle]) && $cle != "rechercher"){
+          if($i != 0)
+            $req = $req." and ";
+          if($cle == "nom"){
+            $nom = traiterNom($_POST["nom"]);
+            $req = $req."$cle='$nom'";
+          }
+          else if($cle == "prenom"){
+            $prenom = traiterPrenom($_POST["prenom"]);
+            $req = $req." $cle='$prenom'";
+          }
+          else $req = $req."$cle='$contenu'";
+          $i++;
+        }
       }
     }
+    else{
+      $req = "SELECT * FROM tdf_coureur";
+    }
+    $req = utf8_decode($req);
+    $cur = PreparerRequete($conn,$req);
+    $res = ExecuterRequete($cur);
+    $nb = LireDonnees1($cur,$donnees);
+    AfficherListe($donnees,$nb);
+    FermerConnexion($conn);
   }
-  else{
-    $req = "SELECT * FROM tdf_coureur";
-  }
-  $req = utf8_decode($req);
-  $cur = PreparerRequete($conn,$req);
-  $res = ExecuterRequete($cur);
-  $nb = LireDonnees1($cur,$donnees);
-  AfficherListe($donnees,$nb);
-  FermerConnexion($conn);
-}
-?>
+  ?>
 
